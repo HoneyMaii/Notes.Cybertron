7d164d7698fd5040abef859dfaa4de6d2cf249dc
EdwardChu <edward@maistudio.com>
2022-07-12 21:10:39 +0800

    增加个人主页信息

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -59,13 +59,16 @@ module.exports = {
         "type": "blog",
         "blogConfig": {
             "category": {
-                "location": 2,
-                "text": "Category"
+                "location": 2, // 在导航栏菜单中所占的位置，默认2
+                "text": "Category" // 默认文案 “分类”
             },
             "tag": {
-                "location": 3,
-                "text": "标签"
-            }
+                "location": 3, // 在导航栏菜单中所占的位置，默认3
+                "text": "标签" // 默认文案 “标签”
+            },
+            socialLinks: [ // 信息栏展示社交信息
+                { icon: 'reco-github', link: 'https://github.com/HoneyMaii' }
+            ]
         },
         "friendLink": [
             // {
